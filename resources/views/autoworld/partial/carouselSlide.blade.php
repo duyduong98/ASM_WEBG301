a833e0e5fa292fde741ec75e6d50bd9ef99d41a0
nhtminhh <minhtngch200030@fpt.edu.vn>
2022-04-21 09:25:59 +0700

    update carosel

---
+++ resources/views/autoworld/partial/carouselSlide.blade.php
@@ -0,0 +1,36 @@
+<div class="slide">
+    <div class="container">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="qc">
+                        <img  src="{{ asset('images\slide-auto\hinh-anh-o-to-con.jpg') }}" alt="First slide">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="qc">
+                        <img src="{{ asset('images\slide-auto\hinh-nen-xe-oto-dep-1.jpg') }}" alt="Second slide">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="qc">
+                        <img src="{{ asset('images\slide-auto\o-to-moi-4.jpg') }}" alt="Third slide">
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</div>
